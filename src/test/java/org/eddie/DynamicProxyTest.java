5cdb6e8d5f0a40bd2a5c849d73c7bb3f94f594ca
eddie_xie <eddie_xie@derbysoft.com>
2018-09-01 17:24:22 +0800

    Init

---
+++ src/test/java/org/eddie/DynamicProxyTest.java
@@ -0,0 +1,58 @@
+package org.eddie;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+public class DynamicProxyTest {
+    private class TimingDynamicInvocationHandler implements InvocationHandler {
+
+        private final Map<String, Method> methods = new HashMap<>();
+
+        private Object target;
+
+        public TimingDynamicInvocationHandler(Object target) {
+            this.target = target;
+
+            for (Method method : target.getClass().getDeclaredMethods()) {
+                this.methods.put(method.getName(), method);
+            }
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            long start = System.nanoTime();
+            Object result = methods.get(method.getName()).invoke(target, args);
+            long elapsed = System.nanoTime() - start;
+
+            System.out.println(String.format("Executing %s finished in %d ns", method.getName(), elapsed));
+
+            return result;
+        }
+    }
+
+    @Test
+    public void testCreatingProxies() throws Exception {
+
+        Map mapProxyInstance = (Map) Proxy.newProxyInstance(
+                DynamicProxyTest.class.getClassLoader(), new Class[]{Map.class},
+                new TimingDynamicInvocationHandler(new HashMap<>()));
+
+        mapProxyInstance.put("hello", "world");
+        mapProxyInstance.get("hello");
+        mapProxyInstance.putIfAbsent("foo", "bar");
+
+        CharSequence csProxyInstance = (CharSequence) Proxy.newProxyInstance(
+                DynamicProxyTest.class.getClassLoader(),
+                new Class[]{CharSequence.class},
+                new TimingDynamicInvocationHandler("Hello World"));
+
+        csProxyInstance.length();
+    }
+}
